30683beb0be3bba2cdd1b8909c267eeedbdef145
farha_eldaba <farhaeldaba@gmail.com>
2025-04-26 23:24:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'onboarding_screen.dart';
+import 'signup_screen.dart';
+import 'login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowCheckedModeBanner: false: يشيل الشريط الأحمر بتاع debug اللي بيظهر فوق على اليمين.
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const OnboardingScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/login': (context) => const LoginScreen(),
+      },
+    );
+  }
+}
